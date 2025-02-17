e4c9588867c7bb34c4ee2babc28534f5ffc44264
RhythmicRhythm <okubadejorh@gmail.com>
2025-02-17 20:29:21 +0100

    +

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,13 +11,6 @@ export default {
   daisyui: {
     themes: [
       "light",
-      {
-        black: {
-          ...daisyUIThemes["black"],
-          primary: "rgb(29, 155, 240)",
-          secondary: "rgb(24, 24, 24)",
-        },
-      },
     ],
   },
 };
